39f2509275d4029c05f7701bc5ad638aeb8c2702
Rahul.Bhave <rahul.bhave@qxf2.com>
2019-10-28 20:04:08 +0530

    added required files

---
+++ conf/api_example_conf.py
@@ -0,0 +1,26 @@
+#Conf for api example
+
+#add_car
+car_details = {'name':'Ertiga','brand':'Maruti','price_range':'8-10 lacs','car_type':'SUV'}
+
+#get_car_details
+car_name_1 = 'figo'
+brand = 'ford'
+
+#update
+car_name_2 = 'Ertiga'
+update_car = {'name':'Ertiga','brand':'Maruti','price_range':'8-10 lacs','car_type':'SUV'}
+
+#register_car
+customer_details = {'customer_name':'Rahul','city':'Mumbai','pincode':'400001'}
+
+#get cars based on car_type
+car_type= 'hatchback'
+
+#authentication details
+user_name = 'eric' 
+password = 'testqxf2'
+
+#invalid auth details
+invalid_user_name = 'unknown'
+invalid_password = 'unknown'
